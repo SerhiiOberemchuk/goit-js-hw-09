902bbeaf328d9aa55ed4964c0f251ae3cf1741b7
SerhiiOberemchuk <gardenitparts@gmail.com>
2023-10-10 19:45:58 +0200

    task01-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+console.log(btnStart);
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+btnStart.addEventListener('click', onClik);
+function onClik() {
+  const colorOneSecond = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.disabled = true;
+  btnStop.addEventListener('click', () => {
+    clearInterval(colorOneSecond);
+    btnStart.disabled = false;
+  });
+}
