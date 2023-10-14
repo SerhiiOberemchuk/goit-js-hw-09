9db73974b92cba8c68dd2311b71d547008066a0f
SerhiiOberemchuk <gardenitparts@gmail.com>
2023-10-14 18:11:24 +0200

    task03-02

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -22,16 +22,10 @@ function onSubmit(event) {
   for (let position = 1; position <= amount; position++) {
     createPromise(position, delay)
       .then(({ position, delay }) => {
-        Notiflix.Notify.success(
-          `✅Fulfilled promise ${position} in ${delay}ms`
-        );
-        // console.log(`✅Fulfilled promise ${position} in ${delay}ms `);
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
-        Notiflix.Notify.failure(
-          `❌ Rejected promise ${position} in ${delay}ms`
-        );
-        // console.log(`❌ Rejected promise ${position} in ${delay}ms `);
+        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
       });
     delay += step;
   }
